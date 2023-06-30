260a2dd19a3e9dd614dcf5fb8960b86618da26b7
Isaac Peña <info@ipenat.com>
2023-06-30 20:50:28 +0200

    endpoints added

---
+++ src/app/api/endpoints/utils.py
@@ -0,0 +1,12 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Depends, HTTPException
+from app import crud, models
+from app.schemas import user
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[user.User])
+def read_utils() -> Any:
+    return {"Hello": "World"}
